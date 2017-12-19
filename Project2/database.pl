56e45c47e6f7753b36e9b3d49c62d0ef07cc6469
chaotixkilla <sergiodias.17@hotmail.com>
2017-12-19 19:42:45 +0000

    1st commit

--- Project2/database.pl
+++ Project2/database.pl
@@ -1,12 +1,12 @@
 %professor(id, nome, areaCientifica, tipoProfessor, preferenciaHorario[varia entre 0 e 100, 0 -> mais carregado no 1º semestre, 50 -> even, 100 -> mais carregado no 2º semestre])
-professor(1, 'Anabela', 'Ciencias da Comunicacao', catedratico, 50).
-professor(2, 'Josefina', 'Astronomia', associado, 0).
-professor(3, 'Bonifacio', 'Arqueologia', auxiliar, 100).
-professor(4, 'Gertrudes', 'Servicos Domesticos', catedratico, 50).
-professor(5, 'Tony Stark', 'Fisica Nuclear', auxiliar, 100).
-professor(6, 'Ricardo', 'Meio Ambiente', associado, 50).
-professor(7, 'Henrique', 'Inteligencia artifical', associado, 50).
-professor(8, 'Jorge', 'Inteligencia artifical', associado, 50).
+professor(1, 'Anabela', 101, 301, 60).
+professor(2, 'Josefina', 102, 302, 0).
+professor(3, 'Bonifacio', 104, 303, 100).
+professor(4, 'Gertrudes', 108, 301, 50).
+professor(5, 'Tony Stark', 116, 303, 100).
+professor(6, 'Ricardo', 117, 302, 50).
+professor(7, 'Henrique', 118, 302, 50).
+professor(8, 'Jorge', 118, 302, 50).
 
 %areaCientifica(id,nome).
 areaCientifica(101, 'Ciencias da Comunicacao').
@@ -20,27 +20,27 @@ areaCientifica(117, 'Meio ambiente').
 areaCientifica(118, 'Inteligencia artifical').
 
 %Unidade Curricular
-%UC(id, nome, AreaCientifica, Praticas 1º Semestre, Teoricas 1º Semestre, Praticas 2º Semestre, Teoricas 2º Semestre)
-unidadeCurricular(201, 'Relacoes Publicas', 101, 20, 5, 20, 5).
-unidadeCurricular(202, 'Cosmologia', 102, 30, 5, 30, 5).
-unidadeCurricular(203, 'Gatologia', 104, 15, 5, 15, 5).
-unidadeCurricular(204, 'Astronomia Estelar', 102, 10, 5, 10, 5).
-unidadeCurricular(205, 'Metodos Numericos', 116, 5, 5, 5, 5).
-unidadeCurricular(206, 'Laboratorio de fisica', 116, 20, 4, 20, 4).
-unidadeCurricular(207, 'Probabilidades e Estatistica', 108, 5, 5, 5, 5).
-unidadeCurricular(208, 'Fisica Termica', 116, 10, 5, 10, 5).
-unidadeCurricular(209, 'Microbiologia', 117, 20, 10, 20, 10).
-unidadeCurricular(210, 'Fisiologia Animal I', 117, 20, 10, 20, 10).
-unidadeCurricular(211, 'Fisiologia Vegetal', 117, 20, 10, 20, 10).
-unidadeCurricular(212, 'Logica computacional', 118, 15, 5, 15, 5).
-unidadeCurricular(213, 'Metodos de apoio a decisao', 108, 5, 5, 5, 5).
-unidadeCurricular(214, 'Basquetebol', 109, 20, 5, 20, 5).
-unidadeCurricular(215, 'Jornais e Revistas', 110, 5, 5, 5, 5).
+%UC(id, nome, AreaCientifica, semestre, horasPraticas, horasTeoricas)
+unidadeCurricular(201, 'Relacoes Publicas', 101, 1, 20, 5).
+unidadeCurricular(202, 'Cosmologia', 102, 1, 30, 5).
+unidadeCurricular(203, 'Gatologia', 104, 1, 15, 5).
+unidadeCurricular(204, 'Astronomia Estelar', 102, 2, 10, 5).
+unidadeCurricular(205, 'Metodos Numericos', 116, 1, 5, 5).
+unidadeCurricular(206, 'Laboratorio de fisica', 116, 1, 20, 4).
+unidadeCurricular(207, 'Probabilidades e Estatistica', 108, 1, 5, 5).
+unidadeCurricular(208, 'Fisica Termica', 116, 2, 10, 5).
+unidadeCurricular(209, 'Microbiologia', 117, 1, 20, 10).
+unidadeCurricular(210, 'Fisiologia Animal I', 117, 2, 20, 10).
+unidadeCurricular(211, 'Fisiologia Vegetal', 117, 2, 20, 10).
+unidadeCurricular(212, 'Logica computacional', 118, 2, 15, 5).
+unidadeCurricular(213, 'Metodos de apoio a decisao', 108, 2, 5, 5).
+unidadeCurricular(214, 'Basquetebol', 109, 2, 20, 5).
+unidadeCurricular(215, 'Jornais e Revistas', 110, 2, 5, 5).
 
-%tipoProfessor(tipo, cargaHoraria)
-tipoProfessor(catedratico, 7).
-tipoProfessor(associado, 8).
-tipoProfessor(auxiliar, 9).
+%tipoProfessor(ID, tipo, cargaHoraria)
+tipoProfessor(301, 'catedratico', 7).
+tipoProfessor(302, 'associado', 8).
+tipoProfessor(303, 'auxiliar', 9).
 
 %Lista de lista de cenas e tal
 %Linhas = Professores
@@ -49,8 +49,9 @@ tipoProfessor(auxiliar, 9).
 %Se o professor nao lecciona essa UC, os seus parametros de Horas Teoricas e Praticas sera 0s
 %A soma dos elementos de uma coluna sera o total das praticas/teoricas, para cada semestre
 
-ProfsUcsBoard([[0, 0, 0, 0, 0],
-		           [0, 0, 0, 0, 0],
-		           [0, 0, 0, 0, 0],
-		           [0, 0, 0, 0, 0],
-		           [0, 0, 0, 0, 0]]).
+%ProfsUcsBoard([[0, 0, 0, 0, 0],
+%		           [0, 0, 0, 0, 0],
+%		           [0, 0, 0, 0, 0],
+%		           [0, 0, 0, 0, 0],
+%		           [0, 0, 0, 0, 0]]).
+%
--- Project2/logic.pl
+++ Project2/logic.pl
@@ -3,11 +3,31 @@
 :-include('database').
 
 
-restrictTeacher(IdProf, NomeProf, AreaCientifica, TipoProf, PreferenciaHorario):-
-  professor(IdProf, NomeProf, AreaCientifica, TipoProf, PreferenciaHorario),
-  tipoProfessor(TipoProf, CargaHoraria),
-  areaCientifica(IdArea, AreaCientifica),
-  unidadeCurricular(IdUni, NomeUni, HorasT, HorasP, IdArea, IdProf),
-  CargaSemanal in 0..CargaHoraria,
-  HorasProfT in 0..HorasT,
-  HorasProfP in 0..HorasP.
+calculateSemesterHours(ProfID, HoursFirstSemester, HoursSecondSemester) :-
+	professor(ProfID, ProfName, _ProfArea, ProfTypeID, ProfDistributionPreference),
+	tipoProfessor(ProfTypeID, _, Hours),
+	HoursFirstSemester is (100 - ProfDistributionPreference)/100 * Hours,
+	HoursSecondSemester is ProfDistributionPreference/100 * Hours,
+	write('Professor '), write(ProfName), write(' is giving '), 
+	write(HoursFirstSemester), write(' weekly hours in the 1st semester, and '), 
+	write(HoursSecondSemester), write(' weekly hours in the 2nd semester.'), nl.
+	
+getAllProfsFromScientificArea(ScientificAreaID, List) :-
+	setof([ProfID, ProfName, ProfScientificAreaID, ProfTypeID, ProfPreference], (professor(ProfID, ProfName, ProfScientificAreaID, ProfTypeID, ProfPreference), ProfScientificAreaID #= ScientificAreaID), List).
+	
+getAllClassesFromScientificArea(ScientificAreaID, List) :-
+	setof([ClassID, ClassName, ClassScientificAreaID, ClassSemester, ClassPracticalHours, ClassTheoricalHours], (unidadeCurricular(ClassID, ClassName, ClassScientificAreaID, ClassSemester, ClassPracticalHours, ClassTheoricalHours), ClassScientificAreaID #= ScientificAreaID), List).
+	
+getAllProfs(List) :-
+	setof([A, B, C, D, E], professor(A, B, C, D, E), List).
+	
+getAllProfsPossibleClasses([]).
+getAllProfsPossibleClasses([[ProfID, ProfName, ProfScientificAreaID, _, _]|T]) :-
+	calculateSemesterHours(ProfID, _HoursFirstSemester, _HoursSecondSemester),
+	getAllClassesFromScientificArea(ProfScientificAreaID, ClassesList),
+	write('Professor '), write(ProfName), write(' can teach '), write(ClassesList), nl, nl,
+	getAllProfsPossibleClasses(T).
+	
+teste :-
+	getAllProfs(List),
+	getAllProfsPossibleClasses(List).
